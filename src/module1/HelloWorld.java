38f1378367abe0571ae9505e41133a34d181905c
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2019-03-06 20:23:27 +0100

    Assignment 1 working

--- src/module1/HelloWorld.java
+++ src/module1/HelloWorld.java
@@ -5,6 +5,8 @@ import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.providers.AbstractMapProvider;
 import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.providers.Microsoft;
+import de.fhpotsdam.unfolding.providers.GeoMapApp;
 import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 
@@ -36,7 +38,7 @@ public class HelloWorld extends PApplet
 	UnfoldingMap map2;
 
 	public void setup() {
-		size(800, 600, P2D);  // Set up the Applet window to be 800x600
+		size(850, 600, P2D);  // Set up the Applet window to be 800x600
 		                      // The OPENGL argument indicates to use the 
 		                      // Processing library's 2D drawing
 		                      // You'll learn more about processing in Module 3
@@ -46,7 +48,7 @@ public class HelloWorld extends PApplet
 		this.background(200, 200, 200);
 		
 		// Select a map provider
-		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
+		AbstractMapProvider provider = new GeoMapApp.TopologicalGeoMapProvider();;
 		// Set a zoom level
 		int zoomLevel = 10;
 		
@@ -76,6 +78,14 @@ public class HelloWorld extends PApplet
 		
 		// TODO: Add code here that creates map2 
 		// Then you'll modify draw() below
+		map2 = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+
+		// The next line zooms in and centers the map at 
+	    // 32.9 (latitude) and -117.2 (longitude)
+	    map2.zoomAndPanTo(zoomLevel, new Location(-22.9f, -43.2f));
+		
+		// This line makes the map interactive
+		MapUtils.createDefaultEventDispatcher(this, map2);
 
 	}
 
@@ -84,6 +94,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
